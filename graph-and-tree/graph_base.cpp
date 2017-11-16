60190b8b3978d37c43615f603c55fb225638dc21
Julianne Hong <julianneho92@gmail.com>
2017-11-16 08:22:53 -0500

    graph

--- graph-and-tree/graph_base.cpp
+++ graph-and-tree/graph_base.cpp
@@ -1,69 +1,77 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <queue>
+#include <stack>
 using namespace std;
 
-class Graph{
-  vector<int>* adjList;
-  int N;
-public:
-  Graph(int N);
-  void addNode(int n1, int n2);
-  void BFS(int s); // queue
-  void DFS(int s, vector<bool>& visited); // recursion
-  int getSize(){return N;}
-};
-
-Graph::Graph(int N){
-  N = N;
-  adjList = new vector<int>[N]();
+void build_graph(int src, int dst, unordered_map<int, unordered_set<int>>* graph_ptr){
+  auto& graph = *graph_ptr;
+  graph[src].insert(dst);
 }
 
-void Graph::addNode(int n1, int n2){
-  adjList[n1].push_back(n2);
+void dfs_recursive(int src, vector<bool>* visited_ptr, const unordered_map<int, unordered_set<int>>& graph){
+  auto& visited = *visited_ptr;
+  visited[src] = true;
+  cout << src << " ";
+  for (auto n : graph.at(src)){
+    if (!visited[n])
+      dfs_recursive(n, visited_ptr, graph);
+  }
 }
 
-void Graph::BFS(int s){
-  vector<bool> visited(getSize(),false);
+void dfs_stack(int src, const unordered_map<int, unordered_set<int>>& graph){
+  stack<int> st;
+  st.push(src);
+  vector<bool> visited(graph.size(), false);
+  while(!st.empty()){
+    src = st.top();
+    st.pop();
+    cout << src << " ";
+    visited[src] = true;
+    for (auto n : graph.at(src)){
+      if (!visited[n])
+        st.push(n);
+    }
+  }
+  cout << endl;
+}
+
+void bfs_queue(int src, const unordered_map<int, unordered_set<int>>& graph){
   queue<int> q;
-  q.push(s);
-  visited[s] = true;
-  cout << s << " ";
-  while(!q.empty()){
-    s = q.front();
+  q.push(src);
+  vector<bool> visited(graph.size(), false);
+  while (!q.empty()){
+    src = q.front();
     q.pop();
-    for (auto n : adjList[s]){
-      if (!visited[n]){
+    cout << src << " " << endl;
+    visited[src] = true;
+    for (auto n : graph.at(src)){
+      if (!visited[n])
+      {
         q.push(n);
-        visited[n] = true;
-        cout << n << " ";
+        cout << "pushed: " << n << endl;
       }
     }
   }
   cout << endl;
 }
 
-void Graph::DFS(int s, vector<bool>& visited){
-  cout << s <<  " ";
-  visited[s] = true;
-  for (auto n : adjList[s]){
-    if(!visited[n])
-      DFS(n, visited);
-  }
-  cout << endl;
-}
-
-int main()
-{
-  Graph g(4);
-  g.addNode(0, 1);
-  g.addNode(0, 2);
-  g.addNode(1, 2);
-  g.addNode(2, 0);
-  g.addNode(2, 3);
-  g.addNode(3, 3);
-  vector<bool> visited(4, false);
-  g.DFS(0, visited);
-  g.BFS(0);
+int main(){
+  unordered_map<int, unordered_set<int>> graph;
+  build_graph(0, 1, &graph);
+  build_graph(0, 2, &graph);
+  build_graph(1, 3, &graph);
+  build_graph(1, 4, &graph);
+  build_graph(2, 5, &graph);
+  build_graph(2, 6, &graph);
+  build_graph(3, NULL, &graph); // unordered_map::at() will throw exception without key
+  build_graph(4, NULL, &graph);
+  build_graph(5, NULL, &graph);
+  build_graph(6, NULL, &graph);
+  //dfs_stack(0, graph);
+  //vector<bool> visited(graph.size(), false);
+  //dfs_recursive(0, &visited, graph);
+  bfs_queue(0, graph);
   return 0;
 }
---
+++ graph-and-tree/maze.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
+
+using namespace std;
+
+typedef enum { WHITE, BLACK } Color;
+
+struct Coordinate {
+  bool operator==(const Coordinate& that) const{
+    return x == that.x && y == that.y; // this->x==that.x && this->y==that.y
+  }
+  int x, y;
+};
+
+bool is_feasible(const Coordinate& coor, const vector<vector<Color>>& maze){
+  return coor.x >= 0 && coor.x < maze.size() && coor.y >= 0 && coor.y < maze.size() && maze[coor.x][coor.y] == WHITE;
+}
+
+bool search_maze_dfs(const Coordinate& src, const Coordinate& dst, vector<vector<Color>>* maze_ptr, vector<Coordinate>* path_ptr){
+  auto& maze = *maze_ptr;
+  if (!is_feasible(src, maze))
+    return false;
+  
+  path_ptr->push_back(src);
+  maze[src.x][src.y] = BLACK; // visited therefore blocked (wall)
+  
+  if (src == dst)
+    return true;
+  
+  const array<array<int, 2>, 4> shifts = {{ {0,1}, {1,0}, {-1,0}, {0, -1} }};
+  
+  for (auto shift : shifts){
+    if (search_maze_dfs({src.x+shift[0], src.y+shift[1]}, dst, maze_ptr, path_ptr))
+      return true;
+  }
+  
+  // if function reaches here, means it can't find a path from this block --> exclude from the path, but marked visited
+  path_ptr->pop_back();
+  return false;
+}
+
+
