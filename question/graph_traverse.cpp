c186e38a1d470bcec4a2649d2ae185e2b6c32d57
Julianne Hong <julianneho92@gmail.com>
2017-12-29 14:14:47 -0500

    trap

--- question/graph_traverse.cpp
+++ question/graph_traverse.cpp
@@ -13,68 +13,72 @@ void build_graph(int src, int dst, unordered_map<int, unordered_set<int>>& graph
 void bfs(int src, const unordered_map<int, unordered_set<int>>& graph){
   queue<int> q;
   q.push(src);
-  vector<bool> queued(graph.size(), false);
+  vector<bool> visited(graph.size(), false);
   visited[src] = true;
-  cout << queued.size() << endl;
   while (!q.empty()){
     int current = q.front();
-    cout << current << endl;
+    cout << current << " ";
     q.pop();
-    visited[node] = true;
     for (auto node : graph.at(current)){
       if (visited[node] == false){
         q.push(node);
-      }
-    }
-  }
-}
-
-void dfs_stack(int src, const unordered_map<int, unordered_set<int>>& graph){
-  stack<int> st;
-  st.push(src);
-  vector<bool> stacked(graph.size(), false);
-  visited[src-1] = true;
-  while (!st.empty()){
-    int current = st.top();
-    cout << current << endl;
-    st.pop();
-    visited[node-1] = true;
-    for (auto node : graph.at(current)){
-      if (visited[node-1] == false){
-        st.push(node);
+        visited[node] = true;
       }
     }
   }
 }
 
 void dfs_recursive(int src, const unordered_map<int, unordered_set<int>>& graph, vector<bool>& visited){
-  cout << src << endl;
-  visited[src-1] = true;
+  cout << src << " ";
+  visited[src] = true;
   for (auto node : graph.at(src)){
-    if (visited[node-1] == false){
+    if (visited[node] == false)
       dfs_recursive(node, graph, visited);
+  }
+}
+
+void dfs_stack(int src, const unordered_map<int, unordered_set<int>>& graph){
+  stack<int> st;
+  st.push(src);
+  vector<bool> visited(graph.size(), false);
+  visited[src] = true;
+  while (!st.empty()){
+    int current = st.top();
+    st.pop();
+    cout << current << " ";
+    for (auto node : graph.at(current)){
+      if (visited[node] == false){
+        st.push(node);
+        visited[node] = true;
+      }
     }
   }
 }
 
-
-
 int main(){
   unordered_map<int, unordered_set<int>> graph;
-  build_graph(1,2, graph);
-  build_graph(1,3, graph);
-  build_graph(1,4, graph);
-  build_graph(1,5, graph);
-  build_graph(2,6, graph);
-  build_graph(2,7, graph);
-  build_graph(3,8, graph);
-  build_graph(3,9, graph);
-  build_graph(4,10, graph);
-  build_graph(4,11, graph);
-  build_graph(4,12, graph);
-  build_graph(5,13, graph);
- // bfs(1, graph);
-  dfs_stack(1,graph);
- 
+
+  build_graph(0, 8, graph);
+  build_graph(8, 2, graph);
+  build_graph(8, 9, graph);
+  build_graph(2, 1, graph);
+  build_graph(2, 4, graph);
+  build_graph(4, 3, graph);
+  build_graph(3, 1, graph);
+  build_graph(1, 9, graph);
+  build_graph(8, 9, graph);
+  build_graph(3, 5, graph);
+  build_graph(1, 7, graph);
+  build_graph(5, 6, graph);
+  build_graph(7, 6, graph);
+  
+  cout << "bfs: ";
+  bfs(2, graph);
+  vector<bool> visited (graph.size(), false);
+  cout << endl << "dfs_recursive: ";
+  dfs_recursive(2, graph, visited);
+  cout << endl << "dfs_stack: ";
+  dfs_stack(2, graph);
+  cout << endl;
   return 0;
 }
