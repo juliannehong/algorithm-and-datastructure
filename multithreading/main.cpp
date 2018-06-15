e0419773a6f62f50da9d4b0baf166350704c9ee4
Julianne Hong <julianneho92@gmail.com>
2018-06-15 19:39:03 -0400

    multithreading event handling

--- multithreading/main.cpp
+++ multithreading/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <thread>
-#include <string>
-
+#include <mutex>
 using namespace std;
-void thread_function(int x, string str)
+
+class Application
 {
-    cout << str << " is " << x << endl;
-}
- 
-int main() 
-{
-    int x = 10;
-    string str = "hello";
-    std::thread threadObj(thread_function, x, str);
-    //cout << threadObj.get_id() << endl;
-    threadObj.join();
+public:
+    Application(){
+        m_bDataLoaded = false;
+    }
+    void loadData(){ // for thread2
+        this_thread::sleep_for(chrono::milliseconds(1000));
+        cout << "Loading from data" << endl;
+        lock_guard<mutex> lock(m_mutex);
+        m_bDataLoaded = true;
+    }
+    void mainTask(){ // for thread1
+        cout << "handshaking" << endl;
+        m_mutex.lock();
+        while (m_bDataLoaded != true){
+            m_mutex.unlock();
+            this_thread::sleep_for(chrono::milliseconds(100));
+            m_mutex.lock();
+        }
+        m_mutex.unlock();
+        cout << "do processing on loaded data" << endl;
+    }
+private:
+    bool m_bDataLoaded;
+    mutex m_mutex;
+};
+
+int main(){
+    Application app;
+    thread thread1(&Application::mainTask, &app);
+    thread thread2(&Application::loadData, &app);
+    thread2.join();
+    thread1.join();
     return 0;
 }
